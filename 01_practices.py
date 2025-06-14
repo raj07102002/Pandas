f9d4b891e02fc716f545578ffc2fcdf5c3da3af5
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-14 23:25:08 +0530

    one

--- 01_practices.py
+++ 01_practices.py
@@ -4,3 +4,15 @@
 # data = {'Name': ['Alice', 'Bob'], 'Age': [25, 30]}
 # df = pd.DataFrame(data)
 # (df)
+
+# Q.2 
+
+# import pandas as pd
+
+# s = pd.Series([10, 20, None, 30])
+# print(s.count())
+# df = pd.DataFrame({
+#     'A': [1, 2, None],
+#     'B': [4, None, 6]
+# })
+# print(df.count())

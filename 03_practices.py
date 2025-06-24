42a7f9089b328fe62489db0cb44b8dfdb537f9f6
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-24 07:15:11 +0530

    push

---
+++ 03_practices.py
@@ -0,0 +1,10 @@
+# Q.1
+# import pandas as pd
+# df = pd.DataFrame({
+#     'OrderID': [101, 102, 103],
+#     'Product': ['Keyboard', 'Mouse', 'Laptop'],
+#     'Quantity': [2, 1, 1]
+# })
+
+# # Get the Quantity column
+# print(df['Quantity'])
